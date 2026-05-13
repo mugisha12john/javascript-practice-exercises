5f99463fc0855fde0ecafc41009161429a9237db
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-13 15:04:14 +0200

    feat: add greet function with ternary operator for greeting messages

---
+++ codeQuality/question2.js
@@ -0,0 +1,11 @@
+function greet(name) {
+//   if (name) {
+//     return "Hello, " + name + "!";
+//   } else {
+//     return "Hello!";
+//   }
+
+// use terinary operator
+return name ? "Hello, " + name + "!" : "Hello!"
+}
+console.log(greet('mugisha'))
