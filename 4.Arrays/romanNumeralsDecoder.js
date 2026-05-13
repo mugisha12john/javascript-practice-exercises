3cdc4f749f5e7793ed466c0e57c29b5129d52ee0
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-13 14:51:55 +0200

    refactor: streamline continue statement in roman numeral decoding logic and add test cases

--- 4.Arrays/romanNumeralsDecoder.js
+++ 4.Arrays/romanNumeralsDecoder.js
@@ -39,7 +39,7 @@ let result=0
   
       if(currentIndex < nextIndex){
         result -= currentIndex;
-        
+        continue
       }
     
     result += currentIndex
@@ -49,3 +49,4 @@ let result=0
 }
 
 console.log(solution('MMVIII'))
+console.log(solution('IV'))
