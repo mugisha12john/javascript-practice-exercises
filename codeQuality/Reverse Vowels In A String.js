1d54cd4ec45335c890ae56e8f00b34d47c160da9
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-15 13:54:49 +0200

    refactor: improve reverseVowels function for better readability and performance

--- codeQuality/Reverse Vowels In A String.js
+++ codeQuality/Reverse Vowels In A String.js
@@ -8,24 +8,36 @@
 
 // Good luck!
 function reverseVowels(str) {
-  const vowel = ["a", "e", "u", "i", "o", "A", "I", "O", "E", "U"];
-  const reversed = str
-    .split("")
-    .reverse()
-    .filter((letter) => vowel.includes(letter));
+  //   const vowel = ["a", "e", "u", "i", "o", "A", "I", "O", "E", "U"];
+  //   const reversed = str
+  //     .split("")
+  //     .reverse()
+  //     .filter((letter) => vowel.includes(letter));
 
-  let index = 0;
-  return str
-    .split("")
-    .map((letter, i) => {
-      if (vowel.includes(letter)) {
-        const word = reversed[index];
-        index = index + 1;
-        return word;
-      }
-      return letter;
-    })
-    .join("");
+  //   let index = 0;
+  //   return str
+  //     .split("")
+  //     .map((letter, i) => {
+  //       if (vowel.includes(letter)) {
+  //         const word = reversed[index];
+  //         index = index + 1;
+  //         return word;
+  //       }
+  //       return letter;
+  //     })
+  //     .join("");
+
+  //refactor my code by applying code quality rules
+    const vowels = "aeuioAUIOE";
+    const reversed = [...str]
+        .filter((letter) => vowels.includes(letter))
+        .reverse();
+    let index = 0;
+    return [...str]
+        .map((char) => {
+        return vowels.includes(char) ? reversed[index++] : char;
+        })
+        .join("");
 }
 console.log(reverseVowels("Hello!"));
 console.log(reverseVowels("Reverse Vowels In A String"));
